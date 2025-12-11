971f18691b91772631ce20f515e58769069fe9b6
pius wanyangu <ndubipius96@gmail.com>
2025-12-11 12:20:33 +0300

    context

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { CountProvider } from '@/context/CountContext';
-import '@/styles/global.css'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layouts/Layout';
 
